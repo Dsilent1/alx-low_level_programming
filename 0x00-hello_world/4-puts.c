3f93c1547492fd14283e2501001a924f17468820
madubenz <emmanuelalaibe7@gmail.com>
2023-03-17 19:15:35 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * main - print "Programming is like building a multilingual 
+ * main - print "Programming is like building a multilingual
  * puzzle, followed by a new line.
  * Return: Always 0
  */
